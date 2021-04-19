492f4621cf09aa20db61042a914985b5e4941907
taiyounari@gmail.com <taiyounari@users.noreply.github.com>
2021-04-19 20:59:01 +0700

    More stuff

--- src/Controller/LoginScreen.java
+++ src/Controller/LoginScreen.java
@@ -2,16 +2,29 @@ package Controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
 
 public class LoginScreen {
-    @FXML private Text actiontarget;
-
+    @FXML private Text textMessage;
+    @FXML private TextField username;
+    @FXML private PasswordField passwordField;
     @FXML protected void handleSubmitButtonAction(ActionEvent event) {
-        actiontarget.setText("Sign in button pressed");
+        if(username.getText().compareTo("")==0)
+            textMessage.setText("Please enter a username!");
+        else if(passwordField.getText().compareTo("")==0)
+            textMessage.setText("Please enter a password!");
+        else if(!login(username.getText(),passwordField.getText()))
+            textMessage.setText("Either the username or password is incorrect");
+
     }
     @FXML protected void handleLoginAsGuestAction(ActionEvent event) {
-        actiontarget.setText("Logged in as guest");
+        login("guest","1");
+    }
+    private Boolean login(String userName, String password){
+        textMessage.setText("Logged in as "+userName);
+        return true;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/View/LoginScreen.fxml"));
+
+        Parent root =
+                FXMLLoader.load(getClass().getResource("/View/LoginScreen.fxml"));
         primaryStage.setTitle("E-Training");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
